eed8c0ffdd027c5534da25ca347408d16ecc6e4f
Faisal Amiruddin <159210623+faisal-amiruddin@users.noreply.github.com>
2024-12-07 12:11:53 +0700

    Update script.js

--- docs/linktree/script.js
+++ docs/linktree/script.js
@@ -1,31 +1,3 @@
-const hoverSound = new Audio('hover_sound.mp3');
-const hoverItems = document.querySelectorAll('.hover-item');
-let currentlyPlaying = null;
-
-hoverItems.forEach(item => {
-  item.addEventListener('mouseenter', () => {
-    if (currentlyPlaying) {
-      hoverSound.pause();
-      hoverSound.currentTime = 0; 
-    }
-
-    currentlyPlaying = item;
-
-    hoverSound.play();
-  });
-
-  item.addEventListener('mouseleave', () => {
-
-    if (currentlyPlaying === item) {
-      hoverSound.pause();
-      hoverSound.currentTime = 0;
-      currentlyPlaying = null;
-    }
-  });
-});
-
-
-
 class PP extends React.Component {
     render() {
         return (
@@ -51,17 +23,44 @@ class Intro extends React.Component {
 }
 
 class Medsos extends React.Component {
-    render() {
-        return (
-            <div class="tombol">
-                <a class="hover-item" target="_blank" href="https://id.linkedin.com/in/faisal-amiruddin-4965292ba"><i class="fab fa-linkedin"></i> LinkedIn</a>
-                <a class="hover-item"  target="_blank" href="https://github.com/Yokaimi"><i class="fab fa-github"></i> Github</a>
-                <a class="hover-item"  target="_blank" href="https://www.instagram.com/faisalamiruddin_"><i class="fab fa-instagram"></i> Instagram</a>
-                <a class="hover-item"  target="_blank" href="https://tiktok.com/@lothic_"><i class="fab fa-tiktok"></i> Tiktok</a>
-            </div>
-        );
-    }
+  componentDidMount() {
+    const hoverSound = new Audio('hover_sound.mp3');
+    const hoverItems = document.querySelectorAll('.hover-item');
+    let currentlyPlaying = null;
+
+    hoverItems.forEach(item => {
+      item.addEventListener('mouseenter', () => {
+        if (currentlyPlaying) {
+          hoverSound.pause();
+          hoverSound.currentTime = 0;
+        }
+
+        currentlyPlaying = item;
+        hoverSound.play();
+      });
+
+      item.addEventListener('mouseleave', () => {
+        if (currentlyPlaying === item) {
+          hoverSound.pause();
+          hoverSound.currentTime = 0;
+          currentlyPlaying = null;
+        }
+      });
+    });
+  }
+
+  render() {
+    return (
+      <div className="tombol">
+        <a className="hover-item" target="_blank" href="https://id.linkedin.com/in/faisal-amiruddin-4965292ba"><i className="fab fa-linkedin"></i> LinkedIn</a>
+        <a className="hover-item" target="_blank" href="https://github.com/Yokaimi"><i className="fab fa-github"></i> Github</a>
+        <a className="hover-item" target="_blank" href="https://www.instagram.com/faisalamiruddin_"><i className="fab fa-instagram"></i> Instagram</a>
+        <a className="hover-item" target="_blank" href="https://tiktok.com/@lothic_"><i className="fab fa-tiktok"></i> Tiktok</a>
+      </div>
+    );
+  }
 }
+
 
 class App extends React.Component {
     render() {
