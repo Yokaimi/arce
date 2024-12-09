699e4465e736565a9950fb156faa72ab2918c992
Faisal Amiruddin <159210623+faisal-amiruddin@users.noreply.github.com>
2024-12-09 10:01:24 +0700

    Add files via upload

---
+++ docs/portofolio/script.js
@@ -0,0 +1,11 @@
+const intro = "kontol";
+const back ="Memek";
+const a = "yaige  uagyvt a gwaye eadyfyrhdvtsaudawewayfgas dagw fyega fwa wahawu waff aef e eaf eafa"
+let itung = 0;
+
+function ganti() {
+    const el = document.getElementById("anjay");
+
+    el.innerHTML = a;
+    itung++;
+}
