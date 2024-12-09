6a31f1b24c32ca25e0053b63687e733ca41d1def
Faisal Amiruddin <159210623+faisal-amiruddin@users.noreply.github.com>
2024-12-09 21:02:07 +0700

    Update script.js

--- docs/portofolio/script.js
+++ docs/portofolio/script.js
@@ -1,11 +1,3 @@
-const intro = "kontol";
-const back ="Memek";
-const a = "yaige  uagyvt a gwaye eadyfyrhdvtsaudawewayfgas dagw fyega fwa wahawu waff aef e eaf eafa"
-let itung = 0;
-
-function ganti() {
-    const el = document.getElementById("anjay");
-
-    el.innerHTML = a;
-    itung++;
+function showAlert() {
+    alert("This feature cannot be used yet");
 }
